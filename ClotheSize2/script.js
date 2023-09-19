a9b5a5110f0ccc2efbf535b8aa307b5f1f744236
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-20 00:16:02 +0800

    updare

--- ClotheSize2/script.js
+++ ClotheSize2/script.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
     let allData = $(".dataTable").html(); // 初始化表格內容
 
     function updateTableContent() {
+        $(".dataTable").html(allData);  // 重置表格內容
         console.log("Updating table content");
         $(".dataTable td:contains('/')").each(function () {
             let text = $(this).text();
@@ -11,6 +12,7 @@ $(document).ready(() => {
             $(this).text($(".switch").hasClass("on") ? inches : centimeters);
         });
     }
+    
 
     updateTableContent();
 
