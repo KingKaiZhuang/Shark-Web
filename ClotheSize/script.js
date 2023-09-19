8a159b474e74a521bae712e04d85ae664ff7d41d
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-19 21:08:38 +0800

    update

--- ClotheSize/script.js
+++ ClotheSize/script.js
@@ -1,21 +1,22 @@
-let originalTableData; // 存儲原始表格內容
+let TableData; // 存儲原始表格內容
 
 // 在文檔準備好後執行
 $(document).ready(function () {
     // 複製原始表格內容
-    originalTableData = $("#sizeTable").html();
+    TableData = $("#sizeTable").html();
     $("#sizeTable td:contains('/')").each(function () {
         let text = $(this).text();
         let newText = text.split(' / ')[0]; // 取得英吋部分
         $(this).text(newText);
+        console.log(this);
     });
 });
 
 // 當切換按鈕狀態改變時
 $("#sizeSwitch").change(function () {
     if (this.checked) {
-        $("#sizeTable").html(originalTableData);
         // 切換按鈕打開，顯示英吋，隱藏公分
+        $("#sizeTable").html(TableData);
         $("#sizeTable td:contains('/')").each(function () {
             let text = $(this).text();
             let newText = text.split(' / ')[1]; // 取得英吋
@@ -23,8 +24,7 @@ $("#sizeSwitch").change(function () {
         });
     } else {
         // 切換按鈕被關閉，顯示原始表格內容
-        $("#sizeTable").html(originalTableData);
-
+        $("#sizeTable").html(TableData);
         $("#sizeTable td:contains('/')").each(function () {
             let text = $(this).text();
             let newText = text.split(' / ')[0]; // 取得公分
